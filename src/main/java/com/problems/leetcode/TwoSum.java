78cdbed0de0cdb5ba27ccc2e65e35753e6034cef
Çetin İmre <ctn.imre@gmail.com>
2020-08-26 00:01:49 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/TwoSum.java
+++ src/main/java/com/problems/leetcode/TwoSum.java
@@ -1,5 +1,8 @@
 package com.problems.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
     public static void main(String[] args) {
@@ -24,6 +27,32 @@ public class TwoSum {
             }
         }
         return indices;
+    }
+
+    public static int[] twoSumTwoPassHashTable(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static int[] twoSumOnePassHashTable(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 
 }
