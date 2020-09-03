4a054e1230c240204a1816a71dba056b491930d7
Çetin İmre <ctn.imre@gmail.com>
2020-09-03 15:29:04 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/IntersectionOfTwoArrays2.java
+++ src/main/java/com/problems/leetcode/IntersectionOfTwoArrays2.java
@@ -1,16 +1,18 @@
 package com.problems.leetcode;
 
+import com.sun.crypto.provider.HmacPKCS12PBESHA1;
+
 import java.util.*;
 
 public class IntersectionOfTwoArrays2 {
     public static void main(String[] args) {
         int[] nums1 = new int[]{1, 2, 2, 1};
-        int[] nums2 = new int[]{2, 2};
-        int[] intersect = intersect(nums1, nums2);
+        int[] nums2 = new int[]{2};
+        int[] intersect = intersectWithHashMap(nums1, nums2);
         Arrays.stream(intersect).forEach(System.out::println);
     }
 
-    public static int[] intersect(int[] nums1, int[] nums2) {
+    public static int[] intersectWithHashMap(int[] nums1, int[] nums2) {
         if (nums1.length==0)
             return new int[]{};
         if (nums2.length==0)
@@ -25,10 +27,32 @@ public class IntersectionOfTwoArrays2 {
             map2.put(nums2[i],map2.getOrDefault(nums2[i],0)+1);
         }
         for (Map.Entry<Integer,Integer> entry:map1.entrySet()){
-            if (map2.getOrDefault(entry.getKey(),0)!=0){
-                for (int i=0;i<entry.getValue();++i)
+                int minValue=Math.min(entry.getValue(),map2.getOrDefault(entry.getKey(),0));
+                for (int i=0;i<minValue;++i)
                     result.add(entry.getKey());
-                map2.put(entry.getKey(),map2.getOrDefault(entry.getKey(),0)-1);
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int[] intersectWithTwoPointers(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0) {
+            return new int[0];
+        }
+        int i = 0;
+        int j = 0;
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> result = new ArrayList<>();
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                result.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]){
+                i++;
+            } else {
+                j++;
             }
         }
         return result.stream().mapToInt(Integer::intValue).toArray();
