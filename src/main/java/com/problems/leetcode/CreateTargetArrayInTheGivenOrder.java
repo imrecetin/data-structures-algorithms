49bce95e0afed1d27c05911d7eb05ad7ae4dffff
Çetin İmre <ctn.imre@gmail.com>
2020-09-03 14:29:33 +0300

    Add Solution

---
+++ src/main/java/com/problems/leetcode/CreateTargetArrayInTheGivenOrder.java
@@ -0,0 +1,11 @@
+package com.problems.leetcode;
+
+public class CreateTargetArrayInTheGivenOrder {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] createTargetArray(int[] nums, int[] index) {
+
+    }
+}
