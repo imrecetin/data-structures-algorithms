c24a668156aa6da3c2485242c0373bfd0199227b
Çetin İmre <ctn.imre@gmail.com>
2020-09-06 14:01:51 +0300

    Add Solution

---
+++ src/main/java/com/problems/leetcode/ValidSudoku.java
@@ -0,0 +1,11 @@
+package com.problems.leetcode;
+
+public class ValidSudoku {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+        return false;
+    }
+}
