3e33cd6defa122b78832fe1ca3125991a67a89a3
Çetin İmre <ctn.imre@gmail.com>
2020-09-06 11:46:54 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/PlusOne.java
+++ src/main/java/com/problems/leetcode/PlusOne.java
@@ -41,4 +41,18 @@ public class PlusOne {
         }
         return results;
     }
+
+    public int[] plusOneSimpleSolution(int[] digits) {
+        int n = digits.length;
+        for(int i=n-1; i>=0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newNumber = new int [n+1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
 }
