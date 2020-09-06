42f63cdd22af3a2dc7d09d4dece33b2c711f422f
Çetin İmre <ctn.imre@gmail.com>
2020-09-06 13:48:53 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/MoveZeroes.java
+++ src/main/java/com/problems/leetcode/MoveZeroes.java
@@ -30,4 +30,31 @@ public class MoveZeroes {
         return nums;
     }
 
+    public void moveZeroesWithTwoPointers2(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+
+        int insertPos = 0;
+        for (int num: nums) {
+            if (num != 0)
+                nums[insertPos++] = num;
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+
+    public void moveZeroesWithTwoPointers(int[] nums) {
+        int pos = 0;
+        for(int i = 0;i < nums.length;i++) {
+            if(nums[i] != 0) {
+                if(i != pos) {
+                    nums[pos] = nums[i];
+                    nums[i] = 0;
+                }
+                pos++;
+            }
+        }
+    }
+
 }
