dcf8e4da590690c576ccc9dbe4234e90a421b149
Çetin İmre <ctn.imre@gmail.com>
2020-09-08 16:12:23 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/string/ReverseString.java
+++ src/main/java/com/problems/leetcode/string/ReverseString.java
@@ -1,11 +1,20 @@
 package com.problems.leetcode.string;
+import java.util.stream.Stream;
 
 public class ReverseString {
     public static void main(String[] args) {
-
+        char[] chars = {'a', 'b', 'c'};
+        reverseString(chars);
+        Stream.of(chars).forEach(System.out::println);
     }
 
-    public void reverseString(char[] s) {
-
+    public static void reverseString(char[] s) {
+        if (s==null || s.length==1)
+            return ;
+        for(int i=0;i<s.length/2;++i){
+            char temp=s[i];
+            s[i]=s[s.length-1-i];
+            s[s.length-1-i]=temp;
+        }
     }
 }
