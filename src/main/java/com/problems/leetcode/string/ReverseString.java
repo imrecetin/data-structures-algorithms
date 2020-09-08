43e7e1b6949cdf04ff559594acf8868a1fd0209d
Çetin İmre <ctn.imre@gmail.com>
2020-09-08 16:25:33 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/string/ReverseString.java
+++ src/main/java/com/problems/leetcode/string/ReverseString.java
@@ -1,4 +1,5 @@
 package com.problems.leetcode.string;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ReverseString {
@@ -16,5 +17,20 @@ public class ReverseString {
             s[i]=s[s.length-1-i];
             s[s.length-1-i]=temp;
         }
+    }
+
+    public static void reverseStringWithStringBuilder(char[] s) {
+        String text=new StringBuilder(String.valueOf(s)).reverse().toString();
+        s=text.toCharArray();
+    }
+
+    //  Divide and Conquer (Recursive)
+    //  O(n log(n))` (Average Case) and `O(n * log(n))` (Worst Case)
+    public static String reverseString(String s) {
+        int length = s.length();
+        if (length <= 1) return s;
+        String leftStr = s.substring(0, length / 2);
+        String rightStr = s.substring(length / 2, length);
+        return reverseString(rightStr) + reverseString(leftStr);
     }
 }
