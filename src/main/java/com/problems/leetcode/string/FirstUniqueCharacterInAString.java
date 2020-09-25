59da1649e5e1f31baf31f80298de5a997c28814e
Çetin İmre <ctn.imre@gmail.com>
2020-09-25 15:59:43 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/string/FirstUniqueCharacterInAString.java
+++ src/main/java/com/problems/leetcode/string/FirstUniqueCharacterInAString.java
@@ -1,9 +1,29 @@
 package com.problems.leetcode.string;
 
+import java.util.HashMap;
+
 public class FirstUniqueCharacterInAString {
 
     public static void main(String[] args) {
         System.out.println(firstUniqChar("aadadaad"));
+        System.out.println(firstUniqCharWithHashMap("aadadaad"));
+    }
+
+    public static int firstUniqCharWithHashMap(String s) {
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int n = s.length();
+        // build hash map : character and how often it appears
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        // find the index
+        for (int i = 0; i < n; i++) {
+            if (count.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
     }
 
     public static int firstUniqChar(String s) {
