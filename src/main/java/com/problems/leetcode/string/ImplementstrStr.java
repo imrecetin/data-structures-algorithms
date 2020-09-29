42440bcb3a53f55be167ff4a2651cd87392b1ed2
Çetin İmre <ctn.imre@gmail.com>
2020-09-29 15:24:50 +0300

    Add Solution

---
+++ src/main/java/com/problems/leetcode/string/ImplementstrStr.java
@@ -0,0 +1,26 @@
+package com.problems.leetcode.string;
+
+public class ImplementstrStr {
+
+    //What should we return when needle is an empty string? This is a great question to ask during an interview.
+    public static void main(String[] args) {
+
+    }
+
+    public int strStr(String haystack, String needle) {
+        if (needle==null || needle=="" ||needle.length()==0)
+            return 0;
+        if (needle.length()>haystack.length())
+            return -1;
+        for (int i=0;i<haystack.length();i++){
+            int j=0;
+            if (haystack.charAt(i)==needle.charAt(j) && (haystack.length()-i)>=needle.length()){
+                for (j=1;j<needle.length() && haystack.charAt(i+j)==needle.charAt(j);++j){ }
+                if (j==needle.length()){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
