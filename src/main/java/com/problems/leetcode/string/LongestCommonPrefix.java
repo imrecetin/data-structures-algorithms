242c00cc6cd850f99478bcc97c2ae78879519e31
Çetin İmre <ctn.imre@gmail.com>
2020-09-30 15:21:41 +0300

    Add Solution

---
+++ src/main/java/com/problems/leetcode/string/LongestCommonPrefix.java
@@ -0,0 +1,12 @@
+package com.problems.leetcode.string;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+
+    }
+
+    //Horizontal scanning
+    public static String longestCommonPrefixV1(String[] strs) {
+
+    }
+}
