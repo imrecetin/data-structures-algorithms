43babaab269994a6a5c40823033c5822080a7c9b
Çetin İmre <ctn.imre@gmail.com>
2020-10-01 09:59:11 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/string/LongestCommonPrefix.java
+++ src/main/java/com/problems/leetcode/string/LongestCommonPrefix.java
@@ -6,7 +6,17 @@ public class LongestCommonPrefix {
     }
 
     //Horizontal scanning
+    //O(S) , where S is the sum of all characters in all strings.
+    //Space complexity : O(1)O(1). We only used constant extra space.
     public static String longestCommonPrefixV1(String[] strs) {
-
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for(int i=1;i<strs.length;++i){
+            while (strs[i].indexOf(prefix)!=0){
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
     }
 }
