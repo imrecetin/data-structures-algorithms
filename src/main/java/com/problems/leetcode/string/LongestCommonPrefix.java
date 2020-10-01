41e2614fe1c0ba0d214dcace5dd05c409f8c50e9
Çetin İmre <ctn.imre@gmail.com>
2020-10-01 10:11:54 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/string/LongestCommonPrefix.java
+++ src/main/java/com/problems/leetcode/string/LongestCommonPrefix.java
@@ -2,7 +2,7 @@ package com.problems.leetcode.string;
 
 public class LongestCommonPrefix {
     public static void main(String[] args) {
-
+        longestCommonPrefixV2(new String[]{"leets","leetcode"});
     }
 
     //Horizontal scanning
@@ -18,5 +18,49 @@ public class LongestCommonPrefix {
             }
         }
         return prefix;
+    }
+
+    //Vertical scanning
+    //Time complexity : O(S)O(S) , where S is the sum of all characters in all strings.
+    //Space complexity : O(1)O(1). We only used constant extra space.
+    public static String longestCommonPrefixV2(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        for (int i = 0; i < strs[0].length() ; i++){
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j ++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c)
+                    return strs[0].substring(0, i);
+            }
+        }
+        return strs[0];
+    }
+
+    //Divide And Conquer
+    //Time complexity : O(S)O(S), where SS is the number of all characters in the array
+    //Space complexity : O(m \cdot \log n)O(m⋅logn)
+    public String longestCommonPrefixV3(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        return longestCommonPrefix(strs, 0 , strs.length - 1);
+    }
+
+    private String longestCommonPrefix(String[] strs, int l, int r) {
+        if (l == r) {
+            return strs[l];
+        }
+        else {
+            int mid = (l + r)/2;
+            String lcpLeft =   longestCommonPrefix(strs, l , mid);
+            String lcpRight =  longestCommonPrefix(strs, mid + 1,r);
+            return commonPrefix(lcpLeft, lcpRight);
+        }
+    }
+
+    String commonPrefix(String left,String right) {
+        int min = Math.min(left.length(), right.length());
+        for (int i = 0; i < min; i++) {
+            if ( left.charAt(i) != right.charAt(i) )
+                return left.substring(0, i);
+        }
+        return left.substring(0, min);
     }
 }
