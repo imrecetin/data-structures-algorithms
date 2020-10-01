dca68860c318819399495f1cd493d0ca25f518c2
Çetin İmre <ctn.imre@gmail.com>
2020-10-01 23:16:53 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/linkedlist/ReverseLinkedList.java
+++ src/main/java/com/problems/leetcode/linkedlist/ReverseLinkedList.java
@@ -1,7 +1,27 @@
 package com.problems.leetcode.linkedlist;
 
+
 public class ReverseLinkedList {
     public static void main(String[] args) {
 
+    }
+
+    //Time complexity : O(n)O(n). Assume that nn is the list's length, the time complexity is O(n)O(n).
+    //Space complexity : O(n)O(n). The extra space comes from implicit stack space due to recursion.
+    //The recursion could go up to nn levels deep.
+    public ListNode reverseListV1(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode temp=reverseListV1(head.next);
+        head.next.next = head;
+        head.next = null;
+        return temp;
+    }
+
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(){}
+        ListNode(int val){this.val=val;}
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 }
