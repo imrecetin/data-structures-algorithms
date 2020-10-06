64c0d008a905da69a9ea2ccafba6af9182afd743
Çetin İmre <ctn.imre@gmail.com>
2020-10-06 12:08:14 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/tree/MaximumDepthOfBinaryTree.java
+++ src/main/java/com/problems/leetcode/tree/MaximumDepthOfBinaryTree.java
@@ -5,5 +5,15 @@ public class MaximumDepthOfBinaryTree {
 
     }
 
+    public static int maxDepth(TreeNode root) {
+        return root == null ? 0 : Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+    }
+
+    public static int maxDepthV2(TreeNode root) {
+        if(root==null) return 0;
+        int lh = maxDepth(root.left); //LST height
+        int rh = maxDepth(root.right);//RST height
+        return Math.max(lh,rh)+1;
+    }
 
 }
