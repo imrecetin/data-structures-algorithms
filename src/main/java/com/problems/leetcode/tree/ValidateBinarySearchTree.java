b0484501b2878efa7225c6b7e125edb796738375
Çetin İmre <ctn.imre@gmail.com>
2020-10-06 12:15:02 +0300

    Add Solution

---
+++ src/main/java/com/problems/leetcode/tree/ValidateBinarySearchTree.java
@@ -0,0 +1,12 @@
+package com.problems.leetcode.tree;
+
+public class ValidateBinarySearchTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isValidBST(TreeNode root) {
+
+    }
+}
