a8bd9aec61edae52d87eb880cae3376318011987
Çetin İmre <ctn.imre@gmail.com>
2020-10-25 23:43:56 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/dynamicprogramming/ClimbingStairs.java
+++ src/main/java/com/problems/leetcode/dynamicprogramming/ClimbingStairs.java
@@ -2,6 +2,7 @@ package com.problems.leetcode.dynamicprogramming;
 
 public class ClimbingStairs {
 
+    //Time complexity : O(2^n). Size of recursion tree will be 2^n
     public int climbStairsBruteForce(int n) {
         return climb_Stairs_BruteForce(0, n);
     }
@@ -15,13 +16,23 @@ public class ClimbingStairs {
         return climb_Stairs_BruteForce(i + 1, n) + climb_Stairs_BruteForce(i + 2, n);
     }
 
-    public int climbStairs(int n) {
-        if(n==1)
+    //Time complexity : O(n). Size of recursion tree can go upto n.
+    //Space complexity : O(n). The depth of recursion tree can go upto n.
+    public int climbStairsRecursionWithMemorization(int n) {
+        int memo[] = new int[n + 1];
+        return climb_Stairs_RecursionWithMemorization(0, n, memo);
+    }
+    public int climb_Stairs_RecursionWithMemorization(int i, int n, int memo[]) {
+        if (i > n) {
+            return 0;
+        }
+        if (i == n) {
             return 1;
-        if (n==2)
-            return 2;
-        if(n==3)
-            return 3;
-        return climbStairs(n-1)+climbStairs(n-2);
+        }
+        if (memo[i] > 0) {
+            return memo[i];
+        }
+        memo[i] = climb_Stairs_RecursionWithMemorization(i + 1, n, memo) + climb_Stairs_RecursionWithMemorization(i + 2, n, memo);
+        return memo[i];
     }
 }
