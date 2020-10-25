ca6c87824e3347b09eb79230c8eb0d2b79135df9
Çetin İmre <ctn.imre@gmail.com>
2020-10-25 23:50:20 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/dynamicprogramming/BestTimeToBuyandSellStock.java
+++ src/main/java/com/problems/leetcode/dynamicprogramming/BestTimeToBuyandSellStock.java
@@ -15,4 +15,18 @@ public class BestTimeToBuyandSellStock {
         }
         return maxprofit;
     }
+
+    //Time complexity : O(n). Only a single pass is needed.
+    //Space complexity : O(1). Only two variables are used.
+    public int maxProfitOnePas(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
 }
