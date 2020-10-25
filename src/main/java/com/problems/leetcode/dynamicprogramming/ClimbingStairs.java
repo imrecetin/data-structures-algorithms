240a11fdc3809b6f1c862bf0d4e8d6a2a678cc26
Çetin İmre <ctn.imre@gmail.com>
2020-10-25 23:46:19 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/dynamicprogramming/ClimbingStairs.java
+++ src/main/java/com/problems/leetcode/dynamicprogramming/ClimbingStairs.java
@@ -35,4 +35,19 @@ public class ClimbingStairs {
         memo[i] = climb_Stairs_RecursionWithMemorization(i + 1, n, memo) + climb_Stairs_RecursionWithMemorization(i + 2, n, memo);
         return memo[i];
     }
+
+    //Time complexity : O(n). Single loop upto n.
+    //Space complexity : O(n). dpdp array of size nn is used.
+    public int climbStairsDynmaicProgramming(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
 }
