0a16ac6c0970e64809f35083715ef2b53bd86aab
Çetin İmre <ctn.imre@gmail.com>
2020-10-27 00:00:49 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/dynamicprogramming/HouseRobber.java
+++ src/main/java/com/problems/leetcode/dynamicprogramming/HouseRobber.java
@@ -18,4 +18,31 @@ public class HouseRobber {
         }
         return Math.max(dp[n - 1][0], dp[n - 1][1]);
     }
+
+    // Space Optimization: Constant space or O(1) space
+    public int robWithSpaceOptimization(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int n = nums.length;
+        //int[][] dp = new int[n][2];
+
+        //dp[0][0] = 0;
+        int previousItemExcluded = 0; // base case
+        //dp[0][1] = nums[0];
+        int previousItemIncluded = nums[0]; // base case
+
+        int currentItemIncluded = 0;
+        int currentItemExcluded = 0;
+
+        for (int i = 1; i < n; i++) {
+            currentItemIncluded = previousItemExcluded + nums[i];
+            currentItemExcluded = Math.max(previousItemIncluded, previousItemExcluded);
+
+            previousItemExcluded = currentItemExcluded;
+            previousItemIncluded = currentItemIncluded;
+        }
+        return Math.max(previousItemExcluded, previousItemIncluded);
+    }
 }
