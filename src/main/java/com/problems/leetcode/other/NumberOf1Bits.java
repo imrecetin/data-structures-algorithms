f5c77f68f509ed0610fbb2e147eb1228b152af69
Çetin İmre <ctn.imre@gmail.com>
2020-10-27 13:27:14 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/other/NumberOf1Bits.java
+++ src/main/java/com/problems/leetcode/other/NumberOf1Bits.java
@@ -24,7 +24,7 @@ public class NumberOf1Bits {
         int sum = 0;
         while (n != 0) {
             sum++;
-            n &= (n - 1);
+            n = n & (n - 1);
         }
         return sum;
     }
