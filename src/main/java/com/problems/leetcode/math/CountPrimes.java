6d76c13048d3b4d46c29634c5ab8b0fb8a317f06
Çetin İmre <ctn.imre@gmail.com>
2020-10-27 11:13:30 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/math/CountPrimes.java
+++ src/main/java/com/problems/leetcode/math/CountPrimes.java
@@ -19,4 +19,24 @@ public class CountPrimes {
         }
         return true;
     }
+
+    public int countPrimesSieveOfEratosthenes(int n) {
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+        // Loop's ending condition is i * i < n instead of i < sqrt(n)
+        // to avoid repeatedly calling an expensive function sqrt().
+        for (int i = 2; i * i < n; i++) {
+            if (!isPrime[i]) continue;
+            for (int j = i * i; j < n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+        return count;
+    }
 }
