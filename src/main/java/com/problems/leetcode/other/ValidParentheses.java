9e22f71f4b2c55b156515ecb2ad2b5973b48eabd
Çetin İmre <ctn.imre@gmail.com>
2020-10-27 16:07:36 +0300

    Add Solution

---
+++ src/main/java/com/problems/leetcode/other/ValidParentheses.java
@@ -0,0 +1,49 @@
+package com.problems.leetcode.other;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParentheses {
+    // Hash table that takes care of the mappings.
+    private HashMap<Character, Character> mappings;
+
+    // Initialize hash map with mappings. This simply makes the code easier to read.
+    public ValidParentheses() {
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')', '(');
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
+    }
+
+    /**
+     * Time complexity : O(n) because we simply traverse the given string one character at a time
+     * and push and pop operations on a stack take O(1) time.
+     * Space complexity : O(n) as we push all opening brackets onto the stack and in the worst case,
+     * we will end up pushing all the brackets onto the stack. e.g.
+     * */
+    public boolean isValid(String s) {
+        // Initialize a stack to be used in the algorithm.
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            // If the current character is a closing bracket.
+            if (this.mappings.containsKey(c)) {
+
+                // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                // If the mapping for this bracket doesn't match the stack's top element, return false.
+                if (topElement != this.mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                // If it was an opening bracket, push to the stack.
+                stack.push(c);
+            }
+        }
+        // If the stack still contains elements, then it is an invalid expression.
+        return stack.isEmpty();
+    }
+}
