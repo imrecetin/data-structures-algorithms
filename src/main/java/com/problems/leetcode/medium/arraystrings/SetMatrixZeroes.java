9d19696f1db8ce8e74cce9d38f2403e9d311459d
Çetin İmre <ctn.imre@gmail.com>
2020-11-03 14:52:54 +0300

    Add Solution

---
+++ src/main/java/com/problems/leetcode/medium/arraystrings/SetMatrixZeroes.java
@@ -0,0 +1,37 @@
+package com.problems.leetcode.medium.arraystrings;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetMatrixZeroes {
+
+    //Additional Memory Approach
+    /**
+     * Time Complexity: O(M×N) where M and N are the number of rows and columns respectively.
+     * Space Complexity: O(M+N).
+     * */
+    public void setZeroes(int[][] matrix) {
+        int R = matrix.length;
+        int C = matrix[0].length;
+        Set<Integer> rows = new HashSet<Integer>();
+        Set<Integer> cols = new HashSet<Integer>();
+
+        // Essentially, we mark the rows and columns that are to be made zero
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
+                }
+            }
+        }
+        // Iterate over the array once again and using the rows and cols sets, update the elements.
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (rows.contains(i) || cols.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
