026e5360dac05f7eacce82448d7ea03e1f105e56
Çetin İmre <ctn.imre@gmail.com>
2020-11-03 15:20:11 +0300

    Add Solution

--- src/main/java/com/problems/leetcode/medium/arraystrings/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/problems/leetcode/medium/arraystrings/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,6 +1,8 @@
 package com.problems.leetcode.medium.arraystrings;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
@@ -50,6 +52,26 @@ public class LongestSubstringWithoutRepeatingCharacters {
             } else {
                 set.remove(s.charAt(i++));
             }
+        }
+        return ans;
+    }
+
+    //Sliding Window Optimized
+    /**
+     * Time complexity : O(n). Index jj will iterate nn times.
+     * Space complexity (HashMap) : O(min(m, n)). Same as the previous approach.
+     * Space complexity (Table): O(m). mm is the size of the charset.
+     * */
+    public int lengthOfLongestSubstringV3(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
         }
         return ans;
     }
