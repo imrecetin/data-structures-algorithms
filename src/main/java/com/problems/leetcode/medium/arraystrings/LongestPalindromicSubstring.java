b7d35c7285f25aebe1d3ae6286f5a6dbc1345a70
Çetin İmre <ctn.imre@gmail.com>
2020-11-05 14:47:24 +0300

    add

---
+++ src/main/java/com/problems/leetcode/medium/arraystrings/LongestPalindromicSubstring.java
@@ -0,0 +1,38 @@
+package com.problems.leetcode.medium.arraystrings;
+
+public class LongestPalindromicSubstring {
+
+    //Expand Around Center
+    /**
+     * Time complexity : O(n^2)
+     * Since expanding a palindrome around its center could take O(n) time, the overall complexity is O(n^2)O(n
+     * Space complexity : O(1)
+     * */
+
+    abba
+    public String longestPalindromeV1(String s) {
+        if (s == null || s.length() < 1)
+            return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
+}
